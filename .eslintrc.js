b24000c2c7b24c44ad754a7931beedff0159523c
sexnine <planz300@gmail.com>
2021-12-18 15:06:54 +1100

    fix dumb fucking linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
