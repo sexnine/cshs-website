22f67f59d26e6719d5444af6af24ae28f1b24feb
0xEquinox <WilliamJMcdonald@protonmail.com>
2021-12-18 00:23:36 -0800

    Added a floating nav bar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1 class="text-xl text-green-500">home</h1>
+  <nav>
+    <ul>
+      <div
+        class="flex text-center px-6 py-6 no-underline bg-red shadow-lg rounded-full object-top mx-10 my-6 justify-center bg-white"
+      >
+        <li class="px-10">Home</li>
+        <li class="px-10">Projects</li>
+        <li class="px-10">Filler</li>
+        <li class="px-10">Filler</li>
+      </div>
+    </ul>
+  </nav>
 </template>
 
 <script>
