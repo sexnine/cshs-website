7d11cf09e617f4b9c188546bf9551ae715e53cc3
sexnine <planz300@gmail.com>
2021-12-18 14:32:18 +1100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-xl text-green-500">home</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {},
+    colors: {
+      red: colors.red,
+      green: colors.green,
+      pink: colors.pink,
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
